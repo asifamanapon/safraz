77b9da14e7b33f0f88481e1ddbfa4e2df824d685
Md. Asif Aman Apon <aponasifaman@gmail.com>
2023-03-20 21:49:37 +0600

    first commit

---
+++ app/Http/Controllers/Demo/DemoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Demo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use App\Models\Blog;
+
+class DemoController extends Controller
+{
+
+    public function HomeMain(){
+        $categories = BlogCategory::orderBy('blog_category','ASC')->get();
+        $allblogs = Blog::latest()->paginate(3);
+        return view('frontend.blog', compact('categories','allblogs'));
+    }
+    
+
+
+    // public function HomeMain(){
+    //     return view('frontend.index');
+    // }// end mehtod 
+
+
+    public function Index(){
+        return view('about');
+    } // end mehtod 
+
+
+    public function ContactMethod(){
+        return view('contact');
+    }
+
+
+}
